5730264f4552599abf96e751541bdb690f768577
Jose R. Guaura <jguaura@gmail.com>
2025-03-30 19:44:04 -0500

    feat: repository pattern

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -5,12 +5,14 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jguaura/gopher_prod/internal/store"
 )
 
 type application struct {
 	config config
+	store store.Storage
 }
 
 type config struct {
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,17 +4,21 @@ import (
 	"log"
 
 	"github.com/jguaura/gopher_prod/internal/env"
+	"github.com/jguaura/gopher_prod/internal/store"
 )
 
 func main() {
 	cfg := config {
 		address: env.GetString("ADDR", ":8081"),
 	}
-
+	
+	store := store.NewStorage(nil)
+	
 	app := application{
 		config: cfg,
+		store: store,
 	}
-
+	
 	mux := app.mount()
 
 	log.Fatal(app.run(mux))
