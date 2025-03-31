d2c353abea13dcb5098d0aaf21a7c14048d3e966
Jose R. Guaura <jguaura@gmail.com>
2025-03-30 20:12:00 -0500

    feat: storage

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -3,12 +3,45 @@ package store
 import (
 	"context"
 	"database/sql"
+
+	"github.com/lib/pq"
 )
 
-type PostsStorage struct {
+type Post struct {
+	ID int64 `json:"id"`
+	Content string `json:"content"`
+	Title string `json:"title"`
+	UserID int64 `json:"user_id"`
+	Tags []string `json:"tags"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
+
+type PostStorage struct {
 	db *sql.DB
 }
 
-func ( *PostsStorage) Create(ctx context.Context) error {
+func (s *PostStorage) Create(ctx context.Context, post *Post) error {
+	query := `
+		INSERT INTO posts (content, title, user_id, tags, created_at, updated_at)
+		VALUES ($1, $2, $3, $4) RETURNING id, created_at, updated_at
+	`
+
+	err :=s.db.QueryRowContext(
+		ctx, 
+		query,
+		post.Content,
+		post.Title,
+		post.UserID,
+		pq.Array(post.Tags),
+	).Scan(
+		&post.ID,
+		&post.CreatedAt,
+		&post.UpdatedAt,
+	)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
